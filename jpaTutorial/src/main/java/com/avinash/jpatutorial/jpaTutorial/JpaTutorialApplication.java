7cead4869ecfb9d4a9d23ad6d13c7050ead087db
avinash <avinashtiwari2405@gmail.com>
2024-10-17 00:22:47 +0530

    gg

--- jpaTutorial/src/main/java/com/avinash/jpatutorial/jpaTutorial/JpaTutorialApplication.java
+++ jpaTutorial/src/main/java/com/avinash/jpatutorial/jpaTutorial/JpaTutorialApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpaTutorialApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(JpaTutorialApplication.class, args);
 	}
